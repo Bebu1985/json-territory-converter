47cc857be3de623ff507cedbf7af8ef1b1219010
Bernhard Buchwinkler <bernhard.buchwinkler@gmx.de>
2018-02-19 21:33:06 +0100

    added control flags, now usable as commandline tool

--- cmd/evaluation/main.go
+++ cmd/evaluation/main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"flag"
+	"path"
+
 	"github.com/Bebu1985/jsonTerritoryConverter/convert"
 	"github.com/Bebu1985/jsonTerritoryConverter/report"
 )
 
-var globalJSONPath = "D:\\Bebu\\Documents\\Versammlung\\Gebiete\\Fieldservice\\ServiceAreaAdministration\\Data\\"
-
 func main() {
 
-	paths := buildSubPaths(globalJSONPath)
+	sp := flag.String("source", "", "data directory of the wolfapps territory program")
+	gp := flag.String("additional", "additional", "path to the additional files for the group informations")
+	dp := flag.String("dest", "result.xlsx", "full filename for the export file as xlsx")
+
+	flag.Parse()
+
+	paths := buildSubPaths(*sp, *gp)
 
 	areaStatus := convert.GetCurrentAreaState(paths)
 
-	report.Excel("ExportFile.xlsx", areaStatus)
+	report.Excel(*dp, areaStatus)
 }
 
-func buildSubPaths(path string) convert.FilePaths {
+func buildSubPaths(dataPath string, subPath string) convert.FilePaths {
 	return convert.FilePaths{
-		ServantFile:    path + "servants.json",
-		GroupFile:      path + "additional\\groups.json",
-		JoinFile:       path + "additional\\groupJoins.json",
-		AreaFile:       path + "areas.json",
-		AreaActionFile: path + "areaactions.json"}
+		ServantFile:    path.Join(dataPath, "servants.json"),
+		GroupFile:      path.Join(dataPath, subPath, "groups.json"),
+		JoinFile:       path.Join(dataPath, subPath, "groupJoins.json"),
+		AreaFile:       path.Join(dataPath, "areas.json"),
+		AreaActionFile: path.Join(dataPath, "areaactions.json")}
 }
